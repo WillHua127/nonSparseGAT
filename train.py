dbd14b7974df1b8a462088c11f9abbde4df1a884
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2020-03-04 16:13:52 -0500

    Add files via upload

---
+++ train.py
@@ -0,0 +1,138 @@
+from __future__ import division
+from __future__ import print_function
+
+import os
+import glob
+import time
+import random
+import argparse
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+
+from utils import load_dataset
+from models import GAT
+
+# Training settings
+parser = argparse.ArgumentParser()
+parser.add_argument('--train_prefix', type=str, default='citeseer', help='prefix identifying training data. cora, pubmed, citeseer.') 
+parser.add_argument('--no-cuda', action='store_true', default=False, help='Disables CUDA training.')
+parser.add_argument('--fastmode', action='store_true', default=False, help='Validate during training pass.')
+parser.add_argument('--seed', type=int, default=72, help='Random seed.')
+parser.add_argument('--epochs', type=int, default=10000, help='Number of epochs to train.')
+parser.add_argument('--lr', type=float, default=0.001, help='Initial learning rate.')
+parser.add_argument('--weight_decay', type=float, default=6e-4, help='Weight decay (L2 loss on parameters).')
+parser.add_argument('--hidden', type=int, default=16, help='Number of hidden units.')
+parser.add_argument('--nb_heads', type=int, default=8, help='Number of head attentions.')
+parser.add_argument('--dropout', type=float, default=0.6, help='Dropout rate (1 - keep probability).')
+parser.add_argument('--alpha', type=float, default=0.5, help='Alpha for the leaky_relu.')
+parser.add_argument('--patience', type=int, default=50, help='Patience')
+parser.add_argument('--runtimes', type=int, default=5, help='Runtim')
+
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+random.seed(args.seed)
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+
+# Load data
+adj, features, labels, idx_train, idx_val, idx_test = load_dataset(args.train_prefix)
+
+if args.cuda:
+    model.cuda()
+    features = features.cuda()
+    adj = adj.cuda()
+    labels = labels.cuda()
+    idx_train = idx_train.cuda()
+    idx_val = idx_val.cuda()
+    idx_test = idx_test.cuda()
+
+features, adj, labels = Variable(features), Variable(adj), Variable(labels)
+
+
+def train(epoch, model, features, labels, idx_train, idx_val, optimizer):
+    t = time.time()    
+    model.train()
+    optimizer.zero_grad()
+    output = model(features, adj)
+    loss_train = F.cross_entropy(output[idx_train], labels[idx_train])
+    acc_train = accuracy(output[idx_train], labels[idx_train])
+    loss_train.backward()
+    optimizer.step()
+
+    model.eval()
+    output = model(features, adj)
+    loss_val = F.cross_entropy(output[idx_val], labels[idx_val])
+    acc_val = accuracy(output[idx_val], labels[idx_val])
+
+    loss_test = F.cross_entropy(output[idx_test], labels[idx_test])
+    acc_test = accuracy(output[idx_test], labels[idx_test])
+    
+    print('Epoch: {:04d}'.format(epoch+1),
+          'loss_train: {:.4f}'.format(loss_train.data.item()),
+          'acc_train: {:.4f}'.format(acc_train),
+          'loss_val: {:.4f}'.format(loss_val.data.item()),
+          'acc_val: {:.4f}'.format(acc_val),
+          'loss_test: {:.4f}'.format(loss_test.data.item()),
+          'acc_test: {:.4f}'.format(acc_test),
+          'time: {:.4f}s'.format(time.time() - t))
+    return loss_val.data.item(), acc_test.data.item()
+
+
+def accuracy(output, labels):
+    preds = output.max(1)[1].type_as(labels)
+    correct = preds.eq(labels).double()
+    correct = correct.sum()
+    return correct / len(labels)
+
+# Train model
+best_tests = []
+for runtime in range(args.runtimes):
+    model = GAT(nfeat=features.shape[1], 
+                nhid=args.hidden, 
+                nclass=int(labels.max()) + 1, 
+                dropout=args.dropout, 
+                nheads=args.nb_heads, 
+                alpha=args.alpha)
+    print("MODEL_BUILT")
+    optimizer = optim.Adam(model.parameters(), 
+                           lr=args.lr, 
+                           weight_decay=args.weight_decay)
+    t_total = time.time()
+    test_acc = []
+    val_loss = []
+    bad_counter = 0
+    best_test = 0
+    best = args.epochs + 1
+    best_epoch = 0
+    for epoch in range(args.epochs):
+        loss, acc = train(epoch, model, features, labels, idx_train, idx_val, optimizer)
+        val_loss.append(loss)
+        test_acc.append(acc)
+
+        if val_loss[-1] < best:
+            best = val_loss[-1]
+            best_test = test_acc[-1]
+            best_epoch = epoch
+            bad_counter = 0
+        else:
+            bad_counter += 1
+
+        if bad_counter == args.patience:
+            break
+            
+    print("Optimization Finished!")
+    print("Total time elapsed: {:.4f}s".format(time.time() - t_total))
+
+    # Restore best model
+    best_tests.append(best_test)
+    print("The best test accuracy this tuntime : ",best_test)
+    del model, optimizer
+print("The average test accuracy : ", np.mean(best_tests), "The test variance : ", np.var(best_tests), "The test variance : ", np.std(best_tests))
+
---
+++ utils.py
@@ -0,0 +1,108 @@
+import numpy as np
+import random
+import json
+import sys
+import os
+import pickle as pkl
+import scipy.sparse as sp
+import networkx as nx
+import math
+import torch 
+
+def parse_index_file(filename):
+    """Parse index file."""
+    index = []
+    for line in open(filename):
+        index.append(int(line.strip()))
+    return index
+
+def load_dataset(dataset, dense=True):
+    dense_adj, features, labels = torch.load("%s_dense_adj.pt" % dataset), torch.load("%s_features.pt" % dataset), torch.load("%s_labels.pt" % dataset)
+
+    y=140
+    #s_val = labels[np.random.choice(labels.shape[0], 500, replace=False)]
+    if dataset == 'citeseer':
+        y=120
+    elif dataset == 'pubmed':
+        y=60
+    idx_train = range(y)
+    #idx_val = range(y, len(s_val) + 500)
+    #idx_test = range(len(s_val) + 500, len(s_val) + 1500)
+    idx_val = range(y, y + 500)
+    idx_test = range(y + 500, y + 1500)
+    idx_train = torch.LongTensor(idx_train)
+    idx_val = torch.LongTensor(idx_val)
+    idx_test = torch.LongTensor(idx_test)
+    if dense:
+        return dense_adj, features, labels, idx_train, idx_val, idx_test
+    else:
+        indices = torch.nonzero(dense_adj).t(); values = dense_adj[indices[0], indices[1]]
+        adj = torch.sparse.FloatTensor(indices, values, dense_adj.size()).clone()
+        del dense_adj, indices, values
+        return adj, features, labels, idx_train, idx_val, idx_test
+    
+
+
+
+def load_data(prefix):
+    print('Loading {} dataset...'.format(prefix))
+    names = ['x', 'y', 'tx', 'ty', 'allx', 'ally', 'graph']
+    objects = []
+    for i in range(len(names)):
+        with open("./dataset/ind.{}.{}".format(prefix, names[i]), 'rb') as f:
+            if sys.version_info > (3, 0):
+                objects.append(pkl.load(f, encoding='latin1'))
+            else:
+                objects.append(pkl.load(f))
+        
+    x, y, tx, ty, allx, ally, graph = tuple(objects)
+    test_idx_reorder = parse_index_file("./dataset/ind.{}.test.index".format(prefix))   
+    test_idx_range = np.sort(test_idx_reorder)
+
+    
+    labels = np.vstack((ally, ty))
+    #labels[test_idx_reorder, :] = labels[test_idx_range, :]
+
+    features = sp.vstack((allx, tx)).tolil()
+    #features[test_idx_reorder, :] = features[test_idx_range, :]
+    features = normalize(features)
+    
+    adj = nx.adjacency_matrix(nx.from_dict_of_lists(graph))
+    adj = sp.csr_matrix(adj, dtype=np.float32)[:,list(np.where(np.sum(labels,1)==1)[0])][list(np.where(np.sum(labels,1)==1)[0]),:]
+    adj = sp.coo_matrix(adj,dtype=np.float32)
+    adj = normalize(adj + sp.eye(adj.shape[0])) 
+
+    # build symmetric adjacency matrix
+    adj = sparse_mx_to_torch_sparse_tensor(adj)
+    features = torch.FloatTensor(np.array(features.todense()))
+    labels = torch.LongTensor(np.where(labels)[1])
+
+    return adj, features, labels, 
+
+
+
+def normalize(mx):
+    """Row-normalize sparse matrix"""
+    rowsum = np.array(mx.sum(1))
+    r_inv = np.power(rowsum, -1).flatten()
+    r_inv[np.isinf(r_inv)] = 0.
+    r_mat_inv = sp.diags(r_inv)
+    mx = r_mat_inv.dot(mx)
+    return mx
+
+def sparse_mx_to_torch_sparse_tensor(sparse_mx):
+    """Convert a scipy sparse matrix to a torch sparse tensor."""
+    sparse_mx = sparse_mx.tocoo().astype(np.float32)
+    indices = torch.from_numpy(
+        np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
+    values = torch.from_numpy(sparse_mx.data)
+    shape = torch.Size(sparse_mx.shape)
+    return torch.sparse.FloatTensor(indices, values, shape)
+
+
+def accuracy(output, labels):
+    preds = output.max(1)[1].type_as(labels)
+    correct = preds.eq(labels).double()
+    correct = correct.sum()
+    return correct / len(labels)
+
