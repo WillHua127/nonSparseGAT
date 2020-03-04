76dbdf6171604c8a4e617009a52a8ab3a15e0cc6
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2020-03-04 16:24:49 -0500

    Update train.py

--- train.py
+++ train.py
@@ -18,19 +18,22 @@ from models import GAT
 
 # Training settings
 parser = argparse.ArgumentParser()
-parser.add_argument('--train_prefix', type=str, default='citeseer', help='prefix identifying training data. cora, pubmed, citeseer.') 
+parser.add_argument('--network', type=str, default='gcn_onepath', help='network model.') 
+parser.add_argument('--public', type=int, default=1, help='split data') 
+parser.add_argument('--dataset', type=str, default='citeseer', help='prefix identifying training data. cora, pubmed, citeseer.') 
 parser.add_argument('--no-cuda', action='store_true', default=False, help='Disables CUDA training.')
 parser.add_argument('--fastmode', action='store_true', default=False, help='Validate during training pass.')
-parser.add_argument('--seed', type=int, default=72, help='Random seed.')
-parser.add_argument('--epochs', type=int, default=10000, help='Number of epochs to train.')
+parser.add_argument('--seed', type=int, default=123, help='Random seed.')
+parser.add_argument('--epochs', type=int, default=500000, help='Number of epochs to train.')
 parser.add_argument('--lr', type=float, default=0.001, help='Initial learning rate.')
 parser.add_argument('--weight_decay', type=float, default=6e-4, help='Weight decay (L2 loss on parameters).')
 parser.add_argument('--hidden', type=int, default=16, help='Number of hidden units.')
-parser.add_argument('--nb_heads', type=int, default=8, help='Number of head attentions.')
-parser.add_argument('--dropout', type=float, default=0.6, help='Dropout rate (1 - keep probability).')
-parser.add_argument('--alpha', type=float, default=0.5, help='Alpha for the leaky_relu.')
-parser.add_argument('--patience', type=int, default=50, help='Patience')
-parser.add_argument('--runtimes', type=int, default=5, help='Runtim')
+parser.add_argument('--nb_heads', type=int, default=16, help='Number of head attentions.')
+parser.add_argument('--dropout', type=float, default=0.7, help='Dropout rate (1 - keep probability).')
+parser.add_argument('--alpha', type=float, default=0.2, help='Alpha for the leaky_relu.')
+parser.add_argument('--patience', type=int, default=200, help='Patience')
+parser.add_argument('--runtimes', type=int, default=20, help='Runtime')
+parser.add_argument('--identifier', type=int, default=1234567, help='Identifier for the job')
 
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
@@ -42,10 +45,9 @@ if args.cuda:
     torch.cuda.manual_seed(args.seed)
 
 # Load data
-adj, features, labels, idx_train, idx_val, idx_test = load_dataset(args.train_prefix)
+adj, features, labels, idx_train, idx_val, idx_test = load_dataset(args.dataset)
 
 if args.cuda:
-    model.cuda()
     features = features.cuda()
     adj = adj.cuda()
     labels = labels.cuda()
@@ -56,18 +58,18 @@ if args.cuda:
 features, adj, labels = Variable(features), Variable(adj), Variable(labels)
 
 
-def train(epoch, model, features, labels, idx_train, idx_val, optimizer):
+def train(epoch, model, features, labels, adj, idx_train, idx_val, optimizer):
     t = time.time()    
     model.train()
     optimizer.zero_grad()
-    output = model(features, adj)
+    output = model(features)
     loss_train = F.cross_entropy(output[idx_train], labels[idx_train])
     acc_train = accuracy(output[idx_train], labels[idx_train])
     loss_train.backward()
     optimizer.step()
 
     model.eval()
-    output = model(features, adj)
+    output = model(features)
     loss_val = F.cross_entropy(output[idx_val], labels[idx_val])
     acc_val = accuracy(output[idx_val], labels[idx_val])
 
@@ -99,11 +101,14 @@ for runtime in range(args.runtimes):
                 nclass=int(labels.max()) + 1, 
                 dropout=args.dropout, 
                 nheads=args.nb_heads, 
-                alpha=args.alpha)
+                alpha=args.alpha,
+                adj=adj)
     print("MODEL_BUILT")
     optimizer = optim.Adam(model.parameters(), 
                            lr=args.lr, 
                            weight_decay=args.weight_decay)
+    if args.cuda:
+        model.cuda()
     t_total = time.time()
     test_acc = []
     val_loss = []
@@ -112,7 +117,7 @@ for runtime in range(args.runtimes):
     best = args.epochs + 1
     best_epoch = 0
     for epoch in range(args.epochs):
-        loss, acc = train(epoch, model, features, labels, idx_train, idx_val, optimizer)
+        loss, acc = train(epoch, model, features, labels, adj, idx_train, idx_val, optimizer)
         val_loss.append(loss)
         test_acc.append(acc)
 
@@ -134,5 +139,5 @@ for runtime in range(args.runtimes):
     best_tests.append(best_test)
     print("The best test accuracy this tuntime : ",best_test)
     del model, optimizer
-print("The average test accuracy : ", np.mean(best_tests), "The test variance : ", np.var(best_tests), "The test variance : ", np.std(best_tests))
-
+print("The average test accuracy : ", np.mean(best_tests), "The test variance : ", np.var(best_tests), "The test standard deviation : ", np.std(best_tests))
+script = open("%d.txt" % args.identifier, 'w'); script.write("%e" % np.mean(best_tests)); script.close()
